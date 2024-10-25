053060562a499a1fc753a068d45efe4a0ce53127
Mirado <hei.mirado@gmail.com>
2024-10-25 14:26:05 +0300

    feat: get monthly fee statistics and list monthly fees

--- src/main/java/school/hei/haapi/repository/dao/FeeDao.java
+++ src/main/java/school/hei/haapi/repository/dao/FeeDao.java
@@ -2,6 +2,7 @@ package school.hei.haapi.repository.dao;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.criteria.*;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.query.QueryUtils;
 import org.springframework.stereotype.Repository;
 import school.hei.haapi.endpoint.rest.model.FeeStatusEnum;
 import school.hei.haapi.model.Fee;
+import school.hei.haapi.model.Mpbs.Mpbs;
 import school.hei.haapi.model.User;
 
 @Repository
@@ -18,25 +20,55 @@ public class FeeDao {
 
   private final EntityManager entityManager;
 
-  public List<Fee> getByCriteria(FeeStatusEnum status, String studentRef, Pageable pageable) {
+  public List<Fee> getByCriteria(
+      FeeStatusEnum status,
+      String studentRef,
+      Instant monthFrom,
+      Instant monthTo,
+      Boolean isMpbs,
+      Pageable pageable) {
     CriteriaBuilder builder = entityManager.getCriteriaBuilder();
     CriteriaQuery<Fee> query = builder.createQuery(Fee.class);
     Root<Fee> root = query.from(Fee.class);
 
     List<Predicate> predicates = new ArrayList<>();
 
-    if (status != null) {
-      predicates.add(builder.equal(root.get("status"), status));
-    }
+    if (status == null
+        && studentRef == null
+        && monthFrom == null
+        && monthTo == null
+        && isMpbs == false) {
+      predicates.add(builder.equal(root.get("status"), FeeStatusEnum.LATE));
+    } else {
+      if (status != null) {
+        predicates.add(builder.equal(root.get("status"), status));
+      }
 
-    if (studentRef != null) {
-      Join<Fee, User> join = root.join("student");
-      predicates.add(builder.like(join.get("ref"), "%" + studentRef + "%"));
+      if (studentRef != null) {
+        Join<Fee, User> join = root.join("student");
+        predicates.add(builder.like(join.get("ref"), "%" + studentRef + "%"));
+      }
+
+      if (monthFrom != null && monthTo != null) {
+        predicates.add(builder.between(root.get("dueDatetime"), monthFrom, monthTo));
+      } else if (monthFrom != null) {
+        predicates.add(builder.greaterThanOrEqualTo(root.get("dueDatetime"), monthFrom));
+      } else if (monthTo != null) {
+        predicates.add(builder.lessThanOrEqualTo(root.get("dueDatetime"), monthTo));
+      }
+
+      if (isMpbs != null) {
+        if (isMpbs) {
+          Join<Fee, Mpbs> mpbsJoin = root.join("mpbs");
+          predicates.add(builder.isNotNull(mpbsJoin));
+          query.orderBy(builder.desc(mpbsJoin.get("creationDatetime")));
+        }
+      }
     }
 
     query
-        .orderBy(QueryUtils.toOrders(pageable.getSort(), root, builder))
-        .where(predicates.toArray(new Predicate[0]));
+        .where(predicates.toArray(new Predicate[0]))
+        .orderBy(QueryUtils.toOrders(pageable.getSort(), root, builder));
 
     return entityManager
         .createQuery(query)
