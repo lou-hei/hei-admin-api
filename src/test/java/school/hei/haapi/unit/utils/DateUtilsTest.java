08e52ce47a44aca447f7f62f302e58038cda8c5e
Mahefaa <hei.mahefa@gmail.com>
2024-12-04 12:28:50 +0300

    chore: DateUtilsTest -> FacadeIT

--- src/test/java/school/hei/haapi/unit/utils/DateUtilsTest.java
+++ src/test/java/school/hei/haapi/unit/utils/DateUtilsTest.java
@@ -1,8 +1,6 @@
 package school.hei.haapi.unit.utils;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-import static school.hei.haapi.integration.conf.TestUtils.anAvailableRandomPort;
 import static school.hei.haapi.integration.conf.TestUtils.setUpCognito;
 import static school.hei.haapi.integration.conf.TestUtils.setUpEventBridge;
 
@@ -13,20 +11,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ContextConfiguration;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import school.hei.haapi.integration.conf.AbstractContextInitializer;
-import school.hei.haapi.integration.conf.MockedThirdParties;
+import school.hei.haapi.integration.conf.FacadeITMockedThirdParties;
 import school.hei.haapi.service.utils.DateUtils;
 import software.amazon.awssdk.services.eventbridge.EventBridgeClient;
 
-@SpringBootTest(webEnvironment = RANDOM_PORT)
 @Testcontainers
-@ContextConfiguration(initializers = DateUtilsTest.ContextInitializer.class)
 @AutoConfigureMockMvc
-public class DateUtilsTest extends MockedThirdParties {
+public class DateUtilsTest extends FacadeITMockedThirdParties {
   @Autowired private DateUtils dateUtils;
   @MockBean private EventBridgeClient eventBridgeClientMock;
 
@@ -59,14 +52,5 @@ public class DateUtilsTest extends MockedThirdParties {
     assertEquals(
         lastDayOfMonth.atTime(23, 59, 59, 999999999).atZone(ZoneOffset.UTC).toInstant(),
         defaultRange[1]);
-  }
-
-  static class ContextInitializer extends AbstractContextInitializer {
-    public static final int SERVER_PORT = anAvailableRandomPort();
-
-    @Override
-    public int getServerPort() {
-      return SERVER_PORT;
-    }
   }
 }
