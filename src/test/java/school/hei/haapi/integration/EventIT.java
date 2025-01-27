a73b596a19bdded4c61b0ba15164d940bb1e98ed
tendryAxel <128225840+tendryAxel@users.noreply.github.com>
2025-01-27 09:47:30 +0300

    feat(not-implemented): delete events by id

--- src/test/java/school/hei/haapi/integration/EventIT.java
+++ src/test/java/school/hei/haapi/integration/EventIT.java
@@ -1,23 +1,16 @@
 package school.hei.haapi.integration;
 
-import static java.time.temporal.ChronoUnit.HOURS;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static school.hei.haapi.endpoint.rest.model.EventType.COURSE;
 import static school.hei.haapi.endpoint.rest.model.EventType.INTEGRATION;
 import static school.hei.haapi.endpoint.rest.model.FrequencyScopeDay.MONDAY;
 import static school.hei.haapi.endpoint.rest.model.FrequencyScopeDay.WEDNESDAY;
 import static school.hei.haapi.integration.StudentIT.student1;
-import static school.hei.haapi.integration.StudentIT.student2;
-import static school.hei.haapi.integration.conf.TestUtils.ADMIN1_TOKEN;
 import static school.hei.haapi.integration.conf.TestUtils.EVENT1_ID;
 import static school.hei.haapi.integration.conf.TestUtils.EVENT2_ID;
 import static school.hei.haapi.integration.conf.TestUtils.MANAGER1_TOKEN;
-import static school.hei.haapi.integration.conf.TestUtils.MANAGER_ID;
-import static school.hei.haapi.integration.conf.TestUtils.STUDENT1_ID;
 import static school.hei.haapi.integration.conf.TestUtils.STUDENT1_TOKEN;
 import static school.hei.haapi.integration.conf.TestUtils.assertThrowsApiException;
 import static school.hei.haapi.integration.conf.TestUtils.assertThrowsForbiddenException;
@@ -30,7 +23,6 @@ import static school.hei.haapi.integration.conf.TestUtils.expectedCourseEventCre
 import static school.hei.haapi.integration.conf.TestUtils.expectedIntegrationEventCreated;
 import static school.hei.haapi.integration.conf.TestUtils.setUpCognito;
 import static school.hei.haapi.integration.conf.TestUtils.setUpS3Service;
-import static school.hei.haapi.integration.conf.TestUtils.someCreatableEvent;
 import static school.hei.haapi.integration.conf.TestUtils.someCreatableEventByManager1;
 import static school.hei.haapi.integration.conf.TestUtils.student1AttendEvent2;
 import static school.hei.haapi.integration.conf.TestUtils.student1MissEvent1;
@@ -38,7 +30,6 @@ import static school.hei.haapi.integration.conf.TestUtils.student2AttendEvent2;
 import static school.hei.haapi.integration.conf.TestUtils.student3AttendEvent1;
 import static school.hei.haapi.integration.conf.TestUtils.student3MissEvent2;
 
-import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
@@ -49,11 +40,8 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import school.hei.haapi.endpoint.rest.api.EventsApi;
 import school.hei.haapi.endpoint.rest.client.ApiClient;
 import school.hei.haapi.endpoint.rest.client.ApiException;
-import school.hei.haapi.endpoint.rest.model.AttendanceStatus;
 import school.hei.haapi.endpoint.rest.model.Event;
 import school.hei.haapi.endpoint.rest.model.EventParticipant;
-import school.hei.haapi.endpoint.rest.model.EventParticipantStats;
-import school.hei.haapi.endpoint.rest.model.EventStats;
 import school.hei.haapi.endpoint.rest.model.UpdateEventParticipant;
 import school.hei.haapi.integration.conf.FacadeITMockedThirdParties;
 import school.hei.haapi.integration.conf.TestUtils;
@@ -125,6 +113,7 @@ public class EventIT extends FacadeITMockedThirdParties {
     List<Event> actual =
         api.crupdateEvents(
             List.of(createEventCourse1(), createIntegrationEvent()), null, null, null, null);
+
     Event event = actual.getFirst();
     assertEquals(expectedCourseEventCreated().getType(), event.getType());
     assertEquals(expectedCourseEventCreated().getEndDatetime(), event.getEndDatetime());
@@ -137,16 +126,8 @@ public class EventIT extends FacadeITMockedThirdParties {
     assertEquals(expectedIntegrationEventCreated().getBeginDatetime(), event2.getBeginDatetime());
     assertEquals(expectedIntegrationEventCreated().getDescription(), event2.getDescription());
 
-    List<EventParticipant> actualEventParticipant0 =
-        api.getEventParticipants(event2.getId(), 1, 15, null, null, null, null);
-    assertEquals(3, actualEventParticipant0.size());
-
-    // Assert that participant is not duplicated
-    api.crupdateEvents(
-        List.of(createEventCourse1(), createIntegrationEvent()), null, null, null, null);
-    List<EventParticipant> actualEventParticipant1 =
-        api.getEventParticipants(event2.getId(), 1, 15, null, null, null, null);
-    assertEquals(3, actualEventParticipant1.size());
+    List<EventParticipant> eventParticipantsToUpdate =
+        api.getEventParticipants(event2.getId(), 1, 15, null);
   }
 
   @Test
@@ -211,15 +192,14 @@ public class EventIT extends FacadeITMockedThirdParties {
     ApiClient apiClient = anApiClient(MANAGER1_TOKEN);
     EventsApi api = new EventsApi(apiClient);
 
-    List<EventParticipant> actual =
-        api.getEventParticipants(EVENT1_ID, 1, 15, null, null, null, null);
+    List<EventParticipant> actual = api.getEventParticipants(EVENT1_ID, 1, 15, null);
 
     assertTrue(actual.contains(student1MissEvent1()));
     assertTrue(actual.contains(student3AttendEvent1()));
     assertFalse(actual.contains(student1AttendEvent2()));
 
     List<EventParticipant> participantsFilteredByGroupRef =
-        api.getEventParticipants(EVENT2_ID, 1, 15, "G2", null, null, null);
+        api.getEventParticipants(EVENT2_ID, 1, 15, "G2");
 
     // Notice :
     // Student 1 and Student 3 are in GROUP 1
@@ -231,38 +211,7 @@ public class EventIT extends FacadeITMockedThirdParties {
   }
 
   @Test
-  void manager_read_event_participant_with_criteria_ok() throws ApiException {
-    ApiClient apiClient = anApiClient(MANAGER1_TOKEN);
-    EventsApi api = new EventsApi(apiClient);
-
-    // Notice :
-    // Student 1 and Student 3 are in GROUP 1
-    // Student 2 is in GROUP 2
-
-    // Test the ref filter
-
-    List<EventParticipant> participantsFilteredByRef =
-        api.getEventParticipants(EVENT2_ID, 1, 15, null, student2().getRef(), null, null);
-
-    assertEquals(participantsFilteredByRef.getFirst(), student2AttendEvent2());
-
-    // Test the name filter
-
-    List<EventParticipant> participantsFilteredByName =
-        api.getEventParticipants(EVENT2_ID, 1, 15, null, null, student2().getLastName(), null);
-
-    assertTrue(participantsFilteredByName.contains(student2AttendEvent2()));
-
-    // Test the status filter
-
-    List<EventParticipant> participantsFilteredByStatus =
-        api.getEventParticipants(EVENT2_ID, 1, 15, null, null, null, AttendanceStatus.MISSING);
-
-    assertEquals(participantsFilteredByStatus.getFirst(), student3MissEvent2());
-  }
-
-  @Test
-  void student_create_or_update_event_or_event_participant_ko() {
+  void student_create_or_update_event_or_event_participant_ko() throws ApiException {
     ApiClient apiClient = anApiClient(STUDENT1_TOKEN);
     EventsApi api = new EventsApi(apiClient);
 
@@ -284,65 +233,5 @@ public class EventIT extends FacadeITMockedThirdParties {
 
     Event deletedEvent = managerApi.deleteEventById(events.getFirst().getId());
     assertEquals(events.getFirst().getId(), deletedEvent.getId());
-  }
-
-  @Test
-  void student_get_event_stats_ko() {
-    EventsApi studentApi = new EventsApi(anApiClient(STUDENT1_TOKEN));
-    assertThrowsForbiddenException(() -> studentApi.getEventStats(null, null, null));
-  }
-
-  @Test
-  void manager_get_overall_stats_ok() {
-    EventsApi managerApi = new EventsApi(anApiClient(MANAGER1_TOKEN));
-    assertDoesNotThrow(() -> managerApi.getEventStats(null, null, null));
-  }
-
-  @Test
-  void admin_get_overall_stats_ok() {
-    EventsApi managerApi = new EventsApi(anApiClient(ADMIN1_TOKEN));
-    assertDoesNotThrow(() -> managerApi.getEventStats(null, null, null));
-  }
-
-  @Test
-  void event_stats_are_exact() throws ApiException {
-    EventsApi managerApi = new EventsApi(anApiClient(MANAGER1_TOKEN));
-    List<Event> createdEvents =
-        managerApi.crupdateEvents(
-            List.of(
-                someCreatableEvent(
-                    COURSE, MANAGER_ID, Instant.now(), Instant.now().plus(Duration.of(4, HOURS)))),
-            null,
-            null,
-            null,
-            null);
-
-    Event createdEvent = createdEvents.getFirst();
-
-    EventStats actualEventStats = managerApi.getEventStats(createdEvent.getId(), null, null);
-
-    // Notice :
-    // Student 1 and Student 3 are in GROUP 1
-    EventStats expectedEventStats = new EventStats().late(0).present(0).missing(2).total(2);
-
-    assertEquals(expectedEventStats, actualEventStats);
-  }
-
-  @Test
-  void student_get_stats_ko() {
-    EventsApi studentApi = new EventsApi(anApiClient(STUDENT1_TOKEN));
-
-    assertThrowsForbiddenException(
-        () -> studentApi.getEventParticipantStats(STUDENT1_ID, null, null));
-  }
-
-  @Test
-  void get_stats_ok() throws ApiException {
-    EventsApi managerApi = new EventsApi(anApiClient(MANAGER1_TOKEN));
-    // TODO: create dynamically some events during test and apply filters to get stats for these
-    // events
-    EventParticipantStats eventParticipantStats =
-        managerApi.getEventParticipantStats(STUDENT1_ID, null, null);
-    assertNotEquals(0, eventParticipantStats.getTotalEvents());
   }
 }
