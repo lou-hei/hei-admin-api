0604373ce84fa7e2d25e1774f898b8b5e1362b6e
Haritiana Adriano <103512767+haritianaadriano@users.noreply.github.com>
2025-01-28 13:21:06 +0300

    chore: use better way to check if participant already in event

--- src/main/java/school/hei/haapi/repository/EventParticipantRepository.java
+++ src/main/java/school/hei/haapi/repository/EventParticipantRepository.java
@@ -10,7 +10,7 @@ import school.hei.haapi.model.EventParticipant;
 
 @Repository
 public interface EventParticipantRepository extends JpaRepository<EventParticipant, String> {
-  Optional<List<EventParticipant>> findByEventIdAndGroupIdAndParticipantId(
+  boolean existsByEventIdAndGroupIdAndParticipantId(
       String eventId, String groupId, String participantId);
 
   Optional<List<EventParticipant>> findAllByEventId(String eventId, Pageable pageable);
