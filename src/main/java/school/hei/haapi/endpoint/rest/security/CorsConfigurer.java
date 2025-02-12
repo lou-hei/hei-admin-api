a206b427695214b1efe283832900baaf7d1c4ae0
Lova Fenohasina Lala RAFANOMEZANTSOA <fenohasinalala@gmail.com>
2025-02-12 18:02:34 +0300

    refactor: cors config

--- src/main/java/school/hei/haapi/endpoint/rest/security/CorsConfigurer.java
+++ src/main/java/school/hei/haapi/endpoint/rest/security/CorsConfigurer.java
@@ -1,35 +1,13 @@
 package school.hei.haapi.endpoint.rest.security;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfigurer implements WebMvcConfigurer {
-  private final String frontendUrl;
-  private final String casdoorUrl;
-
-  public CorsConfigurer(
-      @Value("${casdoor.redirect-url}") String redirectUrl,
-      @Value("${casdoor.endpoint}") String casdoorUrl) {
-    this.frontendUrl = parseOrigin(redirectUrl);
-    this.casdoorUrl = parseOrigin(casdoorUrl);
-  }
-
-  private String parseOrigin(String url) {
-    int protocol = url.startsWith("https://") ? 5 : 4;
-    int slash = url.indexOf('/', protocol + 3);
-    return slash == -1 ? url : url.substring(0, slash);
-  }
-
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry
-        .addMapping("/**")
-        .allowedMethods("*")
-        // .allowedOrigins(frontendUrl, casdoorUrl,"http://localhost:3000")
-        // .allowCredentials(true)
-        .allowedOrigins("*");
+    registry.addMapping("/**").allowedMethods("*");
   }
 }
